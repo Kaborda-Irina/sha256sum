8952f16bc9cbc27a41becc6d5d9254a507e9e2f7
Kaborda <iKaborda@scnsoft.com>
2022-03-30 21:21:30 +0300

    created function with NewScanner

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
+func main() {
+	createSHA256()
+}
+
+func createSHA256() {
+
+	myscanner := bufio.NewScanner(os.Stdin)
+	myscanner.Scan()
+	path := myscanner.Text()
+
+	f, err := os.OpenFile(path, os.O_RDONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%x", h.Sum(nil))
 }
