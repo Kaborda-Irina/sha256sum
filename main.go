e0777521f7381ac77b3c9e02485525f87de8e542
Kaborda <iKaborda@scnsoft.com>
2022-03-30 20:51:05 +0300

    created function hash sum

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
+func main() {
+	createSha256Sum()
+}
+
+func createSha256Sum() {
+	h := sha256.New()
+	s, err := ioutil.ReadFile(os.Args[1])
+	h.Write(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Stdout.WriteString(hex.EncodeToString(h.Sum(nil)))
 }
