fa8fb2fef37d8174084da7e639c58b7e0eb86d57
Kaborda <iKaborda@scnsoft.com>
2022-03-31 18:55:30 +0300

    created function with cuncurrensy

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/Kaborda-Irina/sha256sum/feature_third"
+	"sync"
+)
 
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go feature_third.LookForPath(&wg)
+	wg.Wait()
+	fmt.Println("Goroutines finished executing")
 }
