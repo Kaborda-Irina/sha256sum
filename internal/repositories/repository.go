5d787988a74cc0e1eca20312954db5e965ddf909
Kaborda <iKaborda@scnsoft.com>
2022-04-14 00:42:17 +0300

    update func SaveHashSum and func workerPool

--- internal/repositories/repository.go
+++ internal/repositories/repository.go
@@ -26,9 +26,17 @@ func (hr HashRepository) Ping(_ context.Context) error {
 }
 
 func (hr HashRepository) SaveHashSum(inputHashSum models.HashSum, ctx context.Context) error {
-	query := fmt.Sprintf("INSERT INTO %s (fileName,fullFilePath,hashSum) VALUES ($1,$2,$3)", nameTable)
+	query := fmt.Sprintf(`
+		INSERT INTO %s (fileName,fullFilePath,hashSum, algorithm) 
+		VALUES ($1,$2,$3,$4) 
+		ON CONFLICT (fullFilePath) DO UPDATE 
+		SET fileName=EXCLUDED.fileName, fullFilePath=EXCLUDED.fullFilePath, hashSum=EXCLUDED.hashSum, algorithm= EXCLUDED.algorithm;
+	`, nameTable)
+
+	//INSERT INTO table (id, field, field2)
+	//       SELECT 3, 'C', 'Z'WHERE NOT EXISTS (SELECT 1 FROM table WHERE id=3);
 	hash := fmt.Sprintf("%x", inputHashSum.Hash)
-	_, err := hr.db.Exec(query, inputHashSum.FileName, inputHashSum.FullFilePath, hash)
+	_, err := hr.db.Exec(query, inputHashSum.FileName, inputHashSum.FullFilePath, hash, inputHashSum.Algorithm)
 
 	if err != nil {
 		return err
@@ -38,11 +46,11 @@ func (hr HashRepository) SaveHashSum(inputHashSum models.HashSum, ctx context.Co
 }
 
 func (hr HashRepository) GetHashSum(filePath string, ctx context.Context) (models.HashSumFromDB, error) {
-	query := fmt.Sprintf("SELECT id,fileName,fullFilePath,hashSum FROM %s WHERE fullFilePath=$1", nameTable)
+	query := fmt.Sprintf("SELECT id,fileName,fullFilePath,hashSum,algorithm FROM %s WHERE fullFilePath=$1", nameTable)
 	row := hr.db.QueryRow(query, filePath)
 
 	var newHashSum models.HashSumFromDB
-	err := row.Scan(&newHashSum.Id, &newHashSum.FileName, &newHashSum.FullFilePath, &newHashSum.Hash)
+	err := row.Scan(&newHashSum.Id, &newHashSum.FileName, &newHashSum.FullFilePath, &newHashSum.Hash, &newHashSum.Algorithm)
 	if err != nil {
 		return models.HashSumFromDB{}, err
 	}
@@ -54,7 +62,7 @@ func (hr HashRepository) GetHashSum(filePath string, ctx context.Context) (model
 func (hr HashRepository) delete() {
 	result, err := hr.db.Exec("delete from hashFiles")
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 	fmt.Println(result.RowsAffected())
 }
